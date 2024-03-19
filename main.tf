163022e177b3f482bce1bedbd411a55a0ec4d291
mattayamini <70474154+mattayamini@users.noreply.github.com>
2024-03-19 12:09:44 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -7,29 +7,36 @@ terraform {
   }
 }
 
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  subscription_id =""
-  client_id       =""
-  client_secret   =""
-  tenant_id       =""
 }
+
 resource "azurerm_resource_group" "rg" {
   name     = "res_1"
   location = "East us"
+  lifecycle {
+    prevent_destroy = true
+  }
 }
+
 resource "azurerm_virtual_network" "v1" {
   name                = "first_vnet"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   address_space       = ["10.0.0.0/16"]
+  lifecycle {
+    prevent_destroy = true
+  }
 }
+
 resource "azurerm_subnet" "s1" {
   name                 = "snet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.v1.name
   address_prefixes     = ["10.0.1.0/24"]
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 resource "azurerm_network_interface" "nic1" {
   name                = "first-nic"
@@ -40,6 +47,9 @@ resource "azurerm_network_interface" "nic1" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.s1.id
     private_ip_address_allocation = "Dynamic"
+  }
+  lifecycle {
+    prevent_destroy = true
   }
 }
 resource "azurerm_windows_virtual_machine" "vm" {
@@ -54,6 +64,7 @@ resource "azurerm_windows_virtual_machine" "vm" {
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
+    
   }
 
   source_image_reference {
@@ -61,6 +72,9 @@ resource "azurerm_windows_virtual_machine" "vm" {
     offer     = "WindowsServer"
     sku       = "2016-Datacenter"
     version   = "latest"
+  }
+  lifecycle {
+    prevent_destroy = true
   }
 }  
 resource "azurerm_storage_account" "strg" {
@@ -70,11 +84,17 @@ resource "azurerm_storage_account" "strg" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
   access_tier              = "Hot"
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 resource "azurerm_storage_container" "cnt" {
   name                  = "containerdata"
   storage_account_name  = azurerm_storage_account.strg.name
   container_access_type = "private"
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "azurerm_storage_blob" "example" {
@@ -83,4 +103,23 @@ resource "azurerm_storage_blob" "example" {
   storage_container_name = azurerm_storage_container.cnt.name
   type                   = "Block"
   source                 = "C:\\Users\\user\\OneDrive\\Documents\\string.txt"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+resource "azurerm_resource_group" "rg2" {
+  name     = "${var.rgname}"
+  location = "${var.rgloc}"
+}
+resource "azurerm_virtual_network" "v2" {
+  name                = "${var.virnet}"
+  resource_group_name = "${azurerm_resource_group.rg2.name}"
+  location            = "${azurerm_resource_group.rg2.location}"
+  address_space       = ["${var.addrspace}"]
+}
+resource "azurerm_subnet" "s2" {
+  name                 = "${var.sunet}"
+  resource_group_name  = azurerm_resource_group.rg2.name
+  virtual_network_name = azurerm_virtual_network.v2.name
+  address_prefixes     = ["${var.snetaddrsp}"]
 }
